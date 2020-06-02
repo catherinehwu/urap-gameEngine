8bf3e21c745fce03ff818cb9a9e97c2cc438946e
Catherine Hwu <hwu.catherine@berkeley.edu>
2020-06-02 11:31:25 -0700

    Simplifying code and removing methods that are unused for GUI implementation

--- core/src/edu/berkeley/hygieneheroes/GameEngine.java
+++ core/src/edu/berkeley/hygieneheroes/GameEngine.java
@@ -1,19 +1,20 @@
 package edu.berkeley.hygieneheroes;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
 public class GameEngine {
-
+    // Game Features
     private Dice dice;
     private Board board;
     private ArrayList<Player> playersList;
     private int curTurnIndex;
     private int direction;
     private int numOfPlayers;
-    private Scanner input;
 
+    // Interacting with GUI and User
     private boolean turnComplete;
 
     public GameEngine(int Xrange, int Yrange, int endPosNum) {
@@ -21,8 +22,9 @@ public class GameEngine {
         playersList = new ArrayList<>();
         curTurnIndex = 0;
         direction = 1;
-        input = new Scanner(System.in);
-        dice = new Dice(6); //standard dice with 1~6
+
+        //standard dice with 1~6
+        dice = new Dice(6);
     }
 
     public void addSquare(int num, int sqX, int sqY,
@@ -57,85 +59,34 @@ public class GameEngine {
     }
 
     public void activate(BoardGameEngine gameUI) {
+        // Causes game to resume with a next turn or continuation of previous move
         turnComplete = false;
         Player p = playersList.get(curTurnIndex);
+
         if (p.determiningAction()) {
+            // Dice must be rolled to determine the next action
             turnComplete = p.completeAction(gameUI);
         } else if (p.guiTurn(gameUI)) {
+            // Completes next turn in the game
             turnComplete = true;
         }
 
+        // Only advance turn if turn has completed
         if (turnComplete) {
             advanceTurn();
-        }
-
-//        playersList.get(curTurnIndex).guiTurn();
-//        System.out.println("activate end");
-//        advanceTurn();
-//        System.out.println("activate end2");
-//        p.draw(gameUI);
-    }
-
-    public void setUpPlayers() {
-        System.out.println("Please input number of players (at least 1).");
-        if (input.hasNextLine()) {
-            numOfPlayers = Integer.valueOf(input.nextLine());
-        }
-
-        for (int i = 1; i <= numOfPlayers; i += 1) {
-            String playerName;
-            String playerImage;
-            Scanner playerSetUp;
-            System.out.println("Player " + i + ": Input name and image file separated with a space.");
-            if (input.hasNextLine()) {
-                playerSetUp = new Scanner(input.nextLine());
-                playerName = playerSetUp.next();
-                playerImage = playerSetUp.next();
-                addPlayer(playerName, playerImage, i);
-                System.out.println("Added " + playerName + " with image file " + playerImage + " to the game.");
-            }
-        }
-
-    }
-
-    public void play() {
-        boolean playing = true;
-        while (playing) {
-            if (gameOver()) {
-                Player winner = this.winner();
-                System.out.println(winner.getName() + " won the game!");
-                playing = false;
-            } else {
-                Player current = playersList.get(curTurnIndex);
-                while (current.getSkipTurn()) {
-                    current.turnSkipped();
-                    advanceTurn();
-                    current = playersList.get(curTurnIndex);
-                }
-                this.display();
-                current.turn();
-                advanceTurn();
-            }
-
         }
     }
 
     private void advanceTurn() {
-        System.out.println(curTurnIndex);
         curTurnIndex += (1 * direction);
         while (curTurnIndex < 0) {
             curTurnIndex += numOfPlayers;
-            System.out.println("less than loop");
-            System.out.println(curTurnIndex);
         }
         while (curTurnIndex >= numOfPlayers) {
             curTurnIndex -= numOfPlayers;
-            System.out.println("greater than loop");
-            System.out.println(curTurnIndex);
         }
     }
 
-    // changed to public from Java version
     public boolean gameOver() {
         for (Player p : playersList) {
             if (p.getLocation().equals(board.getEnd())) {
@@ -145,7 +96,6 @@ public class GameEngine {
         return false;
     }
 
-    // changed to public from Java version
     public Player winner() {
         for (Player p : playersList) {
             if (p.getLocation().equals(board.getEnd())) {
@@ -157,7 +107,7 @@ public class GameEngine {
 
     private void sortPlayers() {
         Comparator<Player> comp = new AlphabetComparator();
-        playersList.sort(comp);
+        Collections.sort(playersList, comp);
     }
 
     public Board getBoard() {
@@ -169,27 +119,17 @@ public class GameEngine {
     }
 
     // processing input from System.in
-    public String readLine() {
-
-        // promptHuman();
-        promptApproval();
-
-        if (input.hasNextLine()) {
-            return input.nextLine().trim();
-        } else {
-            return null;
-        }
-    }
-
-    private void promptHuman() {
-        System.out.println("Current turn: " + playersList.get(curTurnIndex).getName());
-        System.out.println("Please input a number for your roll.");
-    }
-
-    private void promptApproval() {
-        System.out.println("Current turn: " + playersList.get(curTurnIndex).getName());
-        System.out.println("Please enter anything to roll the dice.");
-    }
+//    public String readLine() {
+//
+//        // promptHuman();
+//        promptApproval();
+//
+//        if (input.hasNextLine()) {
+//            return input.nextLine().trim();
+//        } else {
+//            return null;
+//        }
+//    }
 
     // output for troubleshooting
     public void display() {
