5de2329d97d2790818eb58cc5cf3ae612271253c
Catherine Hwu <hwu.catherine@berkeley.edu>
2020-07-06 13:05:47 -0700

    Comments for PlayerGroup class

--- core/src/edu/berkeley/hygieneheroes/PlayerGroup.java
+++ core/src/edu/berkeley/hygieneheroes/PlayerGroup.java
@@ -2,6 +2,14 @@ package edu.berkeley.hygieneheroes;
 
 import java.util.ArrayList;
 
+/**
+ * Represents one player in the game.
+ * Stores the collection of Player objects (tokens)
+ * associated with this player. Keeps track of whether
+ * or not this player is AI or Human Player. Keeps track
+ * of whether or not this player should have next
+ * turn skipped.
+ */
 public class PlayerGroup {
     private ArrayList<Player> tokens;
     private String name;
@@ -23,11 +31,18 @@ public class PlayerGroup {
         }
     }
 
+    // Constructor for AI Player
     public PlayerGroup(ArrayList<Player> tokens, String name, Board board, boolean AI) {
         this(tokens, name, board);
         computerPlayer = AI;
     }
 
+    /**
+     * Returns true if this player has finished (moved all pieces
+     * to the finish square).
+     *
+     * @return true if player has successfully moved all square to finish square
+     */
     public boolean finished() {
         for (Player token : tokens) {
             if (!token.getLocation().equals(board.getEnd())) {
@@ -41,6 +56,13 @@ public class PlayerGroup {
         return tokens;
     }
 
+    /**
+     * Returns token number (indexed from 1 - # of Tokens per Player)
+     * but tokens stored in a list indexed from 0 - (# of Tokens per Player - 1).
+     *
+     * @param i - token number to retrieve
+     * @return - returns the token / player object associated with that index
+     */
     public Player getTokenNumber(int i) {
         if (i > tokens.size()) {
             System.out.println("Can't get this token");
@@ -58,6 +80,11 @@ public class PlayerGroup {
         return computerPlayer;
     }
 
+    /**
+     * Sets the current token (the one in play or movement)
+     * to a specific token object.
+     * @param i - index of the token that should be set to current token.
+     */
     public void setCurrentToken(int i) {
         if (i > tokens.size()) {
             System.out.println("Can't get this token");
@@ -66,6 +93,9 @@ public class PlayerGroup {
         currentToken = tokens.get(i - 1);
     }
 
+    /**
+     * Resets current token to null. Happens at the end of a player's turn.
+     */
     public void resetCurrentToken() {
         currentToken = null;
     }
