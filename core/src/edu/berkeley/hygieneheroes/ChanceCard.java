e2bedefbca1fedd31d7340565679ebc0933b3ef5
Catherine Hwu <hwu.catherine@berkeley.edu>
2020-07-15 18:19:36 -0700

    Comments for new Chance Card class

--- core/src/edu/berkeley/hygieneheroes/ChanceCard.java
+++ core/src/edu/berkeley/hygieneheroes/ChanceCard.java
@@ -2,6 +2,12 @@ package edu.berkeley.hygieneheroes;
 
 import java.util.ArrayList;
 
+/**
+ * Class representing a single Chance Card.
+ * Each chance card can have an image, sound, and associated text string.
+ * All chance cards also have a list of actions - although they may or may
+ * not have more than one associated action.
+ */
 public class ChanceCard {
     private String cardImage;
     private String cardSound;
@@ -9,6 +15,13 @@ public class ChanceCard {
     private String defaultSound;
     private ArrayList<String> actions;
 
+    /**
+     * Constructor
+     * @param image - image for this chance card
+     * @param sound - sound to play when this card is drawn
+     * @param text - text to display when card is drawn
+     * @param chanceActions - actions that will occur to the player token when card is drawn
+     */
     public ChanceCard(String image, String sound, String text, ArrayList<String> chanceActions) {
         cardImage = image;
         cardSound = sound;
@@ -16,6 +29,9 @@ public class ChanceCard {
         actions = chanceActions;
     }
 
+    /**
+     * Returns all the actions corresponding to this Chance Card
+     */
     public ArrayList<String> getActions() {
         return actions;
     }
